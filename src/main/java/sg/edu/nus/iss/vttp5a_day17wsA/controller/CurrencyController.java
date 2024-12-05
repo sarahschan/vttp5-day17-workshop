4b6bf9e48662bbf78198aa69c45ca3b60cf612d0
sarahschan <sarahschan@hotmail.com>
2024-12-05 21:43:46 +0800

    Working index.html conversion form

--- src/main/java/sg/edu/nus/iss/vttp5a_day17wsA/controller/CurrencyController.java
+++ src/main/java/sg/edu/nus/iss/vttp5a_day17wsA/controller/CurrencyController.java
@@ -5,15 +5,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import jakarta.validation.Valid;
+import sg.edu.nus.iss.vttp5a_day17wsA.model.ConversionForm;
 import sg.edu.nus.iss.vttp5a_day17wsA.model.Currency;
 import sg.edu.nus.iss.vttp5a_day17wsA.service.CurrencyService;
 
 @Controller
-@RequestMapping()
+@RequestMapping(path={"", "/"})
 public class CurrencyController {
     
     @Autowired
@@ -28,13 +33,39 @@ public class CurrencyController {
     //     return currencies;
     // }
 
+
     @GetMapping()
     public String homePage(Model model){
         
+        // Get the list of currencies and form template
         List<Currency> currencies = currencyService.getCurrencies();
+        ConversionForm form = new ConversionForm();
+            form.setFromCurrency("");   // These makes sure that the placeholders can be seen
+            form.setToCurrency("");
+
+        // Add these attributes to build the form page
         model.addAttribute("currencies", currencies);
-        
+        model.addAttribute("form", form);
+
         return "index";
     }
 
+
+    @PostMapping("/convert")
+    public String handleConversion(@Valid @ModelAttribute("form") ConversionForm form, BindingResult result, Model model){
+        
+        System.out.println("Recieved details: " + form);
+
+        // check if all fields are good
+        if (result.hasErrors()){
+            List<Currency> currencies = currencyService.getCurrencies();
+            model.addAttribute("currencies", currencies);
+            model.addAttribute("form", form);
+
+            return "index";
+        }
+
+        // if all is good
+        
+    }
 }
---
+++ src/main/java/sg/edu/nus/iss/vttp5a_day17wsA/model/ConversionForm.java
@@ -0,0 +1,56 @@
+package sg.edu.nus.iss.vttp5a_day17wsA.model;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public class ConversionForm {
+    
+        @NotNull(message = "Please select a currency")
+    private String fromCurrency;
+
+        @NotNull(message = "Please select a currency")
+    private String toCurrency;
+
+        @NotNull(message = "Please enter an amount")
+        @Positive(message = "Amount must be a positive number")
+    private Double amount;
+
+    
+    public ConversionForm() {
+    }
+
+
+    public String getFromCurrency() {
+        return fromCurrency;
+    }
+
+    public void setFromCurrency(String fromCurrency) {
+        this.fromCurrency = fromCurrency;
+    }
+
+    public String getToCurrency() {
+        return toCurrency;
+    }
+
+    public void setToCurrency(String toCurrency) {
+        this.toCurrency = toCurrency;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+
+    @Override
+    public String toString() {
+        return fromCurrency + "," + toCurrency + "," + amount;
+    }
+
+    
+
+}
